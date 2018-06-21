da587ce206956da25d41e97e5b5d9f9b8ad4165c
Chris B <cbski4@live.com>
2018-06-21 15:52:52 -0500

    cleanup

--- src/actions/dndActions.js
+++ src/actions/dndActions.js
@@ -1,7 +1,6 @@
 import { DRAGGED_AND_DROPPED } from './types';
 
 export const doDnD = (list1, list2) => dispatch => {
-    //console.log(list1[0]);
     list1.forEach(el => {
         console.log(el);
     });
